da4c7eac4992881dd3c25a394066f79877eb239c
zyqq <252545095@qq.com>
2019-03-22 19:20:22 +0800

    fix(create)

--- contracts/Led.sol
+++ contracts/Led.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.17;
 
 contract Led {
-    string public ledStatus = '0';
+    string public ledStatus = "0";
 
     function toggleLedStatus (string memory _ledStatus) public {
         ledStatus = _ledStatus;
